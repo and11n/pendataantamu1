8af53adbe2ff261ae15d0720194a3604dc501cb5
unknown <andinirohayani94@gmail.com>
2024-07-04 08:42:22 +0700

    Upload

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/Dashboard', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/Kurir', function () {
+    return view('lap-kurir');
+})->name('kurir');
+
+Route::get('/Tamu', function () {
+    return view('lap-tamu');
+})->name('tamu');
+
+Route::get('/Pegawai', function () {
+    return view('pegawai');
+})->name('pegawai');
+
+Route::get('/QR', function () {
+    return view('qrcode');
+})->name('qr');
+
+Route::get('/SignIn', function () {
+    return view('signin');
+})->name('signin');
+
+Route::get('/SignUp', function () {
+    return view('signup');
+})->name('signup');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
